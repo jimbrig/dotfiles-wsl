d1ede4c88a9b63091eedb0662152e5101f5b7f74
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-03-17 15:18:21 -0400

    Update install-cargo.sh

--- scripts/dev/scripts/install-cargo.sh
+++ scripts/dev/scripts/install-cargo.sh
@@ -6,4 +6,4 @@ sudo apt-get install -y cargo
 
 echo '\n# Add .cargo to $PATH\nexport PATH="~/.cargo/bin:$PATH"\n' >> ~/.zshrc
 
-cargo install cargo-update
+cargo install cargo-update topgrade git-cliff
